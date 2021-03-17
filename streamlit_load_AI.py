ea8828e35b72d22e1b1e2723f9767c0e112dcac4
Zhicheng <58569058+Xuzhicheng931205@users.noreply.github.com>
2021-03-18 07:56:37 +0800

    Update streamlit_load_AI.py

--- streamlit_load_AI.py
+++ streamlit_load_AI.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 agree = st.checkbox('Show predict power consumption using AI model')
 if agree:
-    model = tf.keras.models.load_model('/model_comsumption.h5')
+    model = tf.keras.models.load_model('./model_comsumption.h5')
     st.write("模型已经加载完了")
